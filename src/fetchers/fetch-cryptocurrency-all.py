9d940c0fab0ab1c5240f98e5c8ac6a4d417da320
Yi-Kuang Chen <twolights@gmail.com>
2017-05-07 21:32:51 +0800

    Added fetch-all script

--- src/fetchers/fetch-cryptocurrency-all.py
+++ src/fetchers/fetch-cryptocurrency-all.py
@@ -4,3 +4,7 @@ currencies = {
     'eth_usd': 'Ethereum to USD',
     'btc_usd': 'Bitcoin to USD',
 }
+
+if __name__ == '__main__':
+    for currency in currencies:
+        cr.fetch_cryptocurrency_rate(currency)
